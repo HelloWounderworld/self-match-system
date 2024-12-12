dc6efd4919510cd1b3abc0970de586b0ec5a579c
Leonardo <leonardo.teramatsu@gmail.com>
2024-12-12 15:08:43 +0900

    Update my note

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,13 +2,13 @@
   <div class="text-center py-5 bg-light">
     <!-- Header Section -->
     <div class="d-flex justify-content-between align-items-center px-4">
-      <img src="./assets/nhk_logo.png" alt="NHK Logo" class="img-fluid" style="height: 40px;" />
+      <img src="./assets/logo.png" alt="NHK Logo" class="img-fluid" style="height: 40px;" />
     </div>
 
     <!-- Main Content -->
     <div class="mt-4">
       <!-- Election Logo -->
-      <img src="./assets/nhk_logo.png" alt="Election 2024" class="img-fluid mb-3" style="height: 80px;" />
+      <img src="./assets/logo.png" alt="Election 2024" class="img-fluid mb-3" style="height: 80px;" />
       <h2 class="h5">人気番組 ボートマッチ</h2>
       <img src="./assets/vote_logo.png" alt="Vote Icon" class="img-fluid my-4" style="width: 150px; height: auto;" />
       <p>あなたの好きな番組をお選びください。</p>
