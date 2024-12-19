56c018fd2af39bf305cea965c3b433518fad0c43
Leonardo <leonardo.teramatsu@gmail.com>
2024-12-19 14:42:12 +0900

    Update my note

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,25 +1,21 @@
-import Vue from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
-import VueRouter from 'vue-router';
-import Home from './components/Home.vue';
-import Themes from './components/Themes.vue';
-import Results from './components/Results.vue';
-
-Vue.config.productionTip = false;
-
-Vue.use(VueRouter);
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from './components/HomePage.vue';
+import ThemesPage from './components/ThemesPage.vue';
+import ResultsPage from './components/ResultsPage.vue';
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/themes', component: Themes },
-  { path: '/results', component: Results }
+    { path: '/', component: HomePage },
+    { path: '/themes', component: ThemesPage },
+    { path: '/results', component: ResultsPage }
 ];
 
-const router = new VueRouter({
-  routes
+const router = createRouter({
+    history: createWebHistory(),
+    routes
 });
 
-new Vue({
-  render: h => h(App),
-  router
-}).$mount('#app');
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
